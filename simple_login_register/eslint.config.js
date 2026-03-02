46e5736cc500ebf5e6bdd8152df6f8567e0a9feb
harykurniawan7723 <harykurniawan7723@gmail.com>
2026-03-02 14:18:33 +0700

    aktifkan eslint rules security

--- simple_login_register/eslint.config.js
+++ simple_login_register/eslint.config.js
@@ -4,6 +4,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import security from 'eslint-plugin-security'
+import noUnsanitized from 'eslint-plugin-no-unsanitized'
+import react from 'eslint-plugin-react'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +17,18 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      security.configs.recommended,
+      noUnsanitized.configs.recommended,
     ],
+    plugins: {
+      react,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      'react/no-danger': 'warn',
     },
   },
 ])
